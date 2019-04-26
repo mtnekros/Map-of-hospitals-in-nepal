d182fc4838bc3162f29df74dfa04c21abbde64e0
Diwas Tamang <mtnekros@gmail.com>
2019-04-26 22:21:39 +0545

    Delete practise.py

--- practise.py
+++
@@ -1,9 +0,0 @@
-from vega_datasets import data
-import altair
-
-data = altair.topo_feature(r'data\districts.json',feature='nepal')
-
-dict = data.to_dict()
-
-for key in dict.items():
-    print key
